c925a3842f9c42f8ce0ede34b9efc67dad651e32
tapader13 <minhajtapader0@gmail.com>
2024-11-18 12:09:55 -0500

    mobile responsive done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 import useAuth from './../hooks/useAuth';
+import { TiThMenuOutline } from 'react-icons/ti';
+import { useState } from 'react';
 
 const Navbar = () => {
   const { user, logoutUser } = useAuth();
-
+  const [sidebar, setSidebar] = useState(false);
   const handleLogout = () => {
     logoutUser();
   };
@@ -16,7 +18,7 @@ const Navbar = () => {
         </Link>
 
         {/* Navigation Links */}
-        <div className='flex items-center space-x-6'>
+        <div className='sm:flex hidden  items-center space-x-6'>
           <Link to='/' className='hover:text-gray-300'>
             Home
           </Link>
@@ -35,12 +37,16 @@ const Navbar = () => {
         <div className='flex items-center space-x-4'>
           {user ? (
             <div className='flex gap-5 items-center'>
-              <img
-                title={user?.displayName}
-                src={user?.photoURL}
-                alt='User Profile'
-                className='w-10 h-10 rounded-full cursor-pointer'
-              />
+              <div
+                className='tooltip tooltip-left tooltip-primary'
+                data-tip={user?.displayName}
+              >
+                <img
+                  src={user?.photoURL}
+                  alt='User Profile'
+                  className='w-10 h-10  rounded-full cursor-pointer'
+                />
+              </div>
 
               <div className=''>
                 <button
@@ -60,6 +66,55 @@ const Navbar = () => {
             </Link>
           )}
         </div>
+        <div className='sm:hidden'>
+          <TiThMenuOutline
+            onClick={() => setSidebar(!sidebar)}
+            className='text-3xl cursor-pointer '
+          />
+        </div>
+        {sidebar && (
+          <div className='fixed top-0 right-0 h-full bg-[#282520] w-64 z-50 shadow-lg p-4'>
+            <button
+              className='text-gray-300 text-lg mb-6'
+              onClick={() => setSidebar(false)}
+            >
+              Close
+            </button>
+            <ul className='flex flex-col space-y-4'>
+              <li>
+                <Link
+                  to='/'
+                  className='hover:text-gray-300'
+                  onClick={() => setSidebar(false)}
+                >
+                  Home
+                </Link>
+              </li>
+              {user && (
+                <>
+                  <li>
+                    <Link
+                      to='/updateprofile'
+                      className='hover:text-gray-300'
+                      onClick={() => setSidebar(false)}
+                    >
+                      Update Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to='/myprofile'
+                      className='hover:text-gray-300'
+                      onClick={() => setSidebar(false)}
+                    >
+                      User Profile
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   );
